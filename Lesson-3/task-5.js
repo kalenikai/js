d677ac6ed58c30581458101e283320a1c40d3c1a
kalenikai <69119807+kalenikai@users.noreply.github.com>
2021-03-27 15:34:14 +0300

    Update task-5.js

--- Lesson-3/task-5.js
+++ Lesson-3/task-5.js
@@ -1,5 +1,5 @@
 // Нарисовать пирамиду с 20 рядами с помощью console.log, как показано на рисунке:
-for (let i = 1; i <= 19; i++) {
+for (let i = 1; i <= 20; i++) {
     let symbols = '';
     for (let j = 1; j <= i; j++) {
         symbols += '*';
